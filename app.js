c6267d5cca94561dbe3ab9727b9126acc7ba6210
Hikari-54 <climberte@yandex.ru>
2023-04-09 19:03:31 +0300

    LocalStorage (add, remove, syncHostToStorage)

--- app.js
+++ app.js
@@ -1,46 +1,108 @@
 // Счётчик для индекса задач в LocalStorage
 let numOfTasksInStorage = 0;
 // Копия задач из LocalStorage
-const tasksInStorage = {};
+let tasksInStorage = {};
 
-const inputTask = document.querySelector(".input-task");
 
-// Загрузка задач из LocalStorage
+// Загрузка задач из LocalStorage - вызывается при загрузке DOM
 function syncHostToStorage(){
+    // Если задачи существуют
+    if(localStorage.key("TasksInStorage") != null){
+        // Получаем задачи из LocalStorage
+        tasksInStorage = JSON.parse(localStorage.getItem("TasksInStorage"));
 
+        // Добавление всех задач из localStorage в DOM
+        for (key in tasksInStorage) {
+            
+            let currentTaskFromStorage = tasksInStorage[key];
+            
+            // Замена текста задачи в шаблоне
+            textOfTaks.textContent = currentTaskFromStorage;
+            // Копия заполненного шаблона
+            let templateCopy = template.content.cloneNode(true);
+            // Добавляем новую задачу
+            taskList.append(templateCopy);
+
+            // Обновляем счётчик для задач
+            numOfTasksInStorage = key;
+        }
+        numOfTasksInStorage++;
+    } 
 }
+// При загрузке DOM задачи из localStorage добавляются на страницу
+addEventListener("DOMContentLoaded", syncHostToStorage);
+
+
+// Input ввода текста задачи
+const inputTask = document.querySelector(".input-task");
+// Контейнер для списка задач
+const taskList = document.querySelector(".task-list");
+// Шаблон задачи
+const template = document.querySelector("template");
+// Тег текста задачи в шаблоне
+const textOfTaks = template.content.querySelector("p");
 // Добавление задачи
 function addTask(){
     if (inputTask.value != ""){
-        // Контейнер для списка задач
-        const taskList = document.querySelector(".task-list");
-        // Шаблон задачи
-        const template = document.querySelector("template");
-        // Тег текста задачи
-        const textOfTaks = template.content.querySelector("p");
         // Вставка текста из input'a
         textOfTaks.textContent = inputTask.value;
         // Копия заполненного шаблона
         let templateCopy = template.content.cloneNode(true);
         // Добавляем новую задачу
         taskList.append(templateCopy);
+
+        // Добавление в LocalStorage
+        tasksInStorage[numOfTasksInStorage] = inputTask.value;
+        numOfTasksInStorage++;
+        localStorage.setItem("TasksInStorage", JSON.stringify(tasksInStorage));
+
         // Удаляем введённый текст в input
         inputTask.value = "";
     }
 }
-// Удаление задачи
-function deleteTask(){
-
-}
-
 // Обработка события добавления задачи
 const addTaskButton = document.querySelector(".addTaskButton");
 addTaskButton.addEventListener("click", addTask);
 
-// Обработка события удаления задачи
+
+// Выполнение задачи - вызывается из HTML кода
+function taskDone(elem){
+    const taskBlock = elem.parentNode;
+    // Если задача не выполнена
+    if(taskBlock.querySelector(".taskDoneImg").style.opacity == 0){
+        taskBlock.querySelector(".taskDoneImg").style.opacity = 1;
+        taskBlock.querySelector("p").style.color = "#A8A8B4";
+        taskBlock.querySelector("p").style.textDecoration = "line-through #A8A8B4";
+    }else{
+        taskBlock.querySelector(".taskDoneImg").style.opacity = 0;
+        taskBlock.querySelector("p").style.color = "#575767";
+        taskBlock.querySelector("p").style.textDecoration = "none";
+    }
+
+}
 
 
+// Удаление задачи - вызывается из HTML кода
+function deleteTask(elem){
+    // Получение конкретной задачи
+    const currentTask = elem.parentNode;
+    // Текст задачи для поиска ключа
+    let textOfTask = currentTask.querySelector("p").textContent;
+    // Массив ключей всех задач
+    let arrOfKeys = Object.keys(tasksInStorage);
 
+    // Ищем ключ по тексту задачи
+    for (let numOfKeyInArr in arrOfKeys){
+        if (textOfTask === tasksInStorage[arrOfKeys[numOfKeyInArr]]){
+            // Удаляем найденное свойство
+            delete tasksInStorage[arrOfKeys[numOfKeyInArr]];
+            // Вносим новый объект в localStorage, заменяя им прошлую версию
+            localStorage.setItem("TasksInStorage", JSON.stringify(tasksInStorage));
+        }
+    }
+    // Удаление задачи из DOM дерева
+    currentTask.remove();
+}
 
 
 
